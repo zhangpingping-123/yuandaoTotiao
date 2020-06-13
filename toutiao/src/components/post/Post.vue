b2fa0be21451b984f3f43adc0c67acdce7dab4a7
unknown <2569228717@qq.com>
2020-06-13 18:07:48 +0800

    上传图片

--- toutiao/src/components/post/Post.vue
+++ toutiao/src/components/post/Post.vue
@@ -1,30 +1,40 @@
 <!-- 切换栏 -->
 <template>
-<div class='post-box'>
+  <div class="post-box">
     <!-- tab切换开始 -->
     <div class="post-tabs">
-        <div :class="['tabs-item',{'active':tab.id==activeTab}]" 
-            v-for="tab in tabs" :key="tab.id"
-            @click.stop="clickTab(tab.id)"
-        >
-            {{tab.text}}
-        </div>
+      <div
+        :class="['tabs-item',{'active':tab.id==activeTab}]"
+        v-for="tab in tabs"
+        :key="tab.id"
+        @click.stop="clickTab(tab.id)"
+      >{{tab.text}}</div>
     </div>
     <!-- tab切换结束 -->
     <!-- 文章输入开始 -->
     <div class="post-text" v-show="activeTab=='wTouTiao'">
-        <textarea class="text-input" name="" id="" cols="30" rows="10" placeholder="有什么新鲜事告诉大家"></textarea>
-        <div class="bottom">
-            <div class="imgButton">
-                <i class="iconfont icon-picture"></i>
-                <div class="imgButton-tiitle" @click.stop="toggleUpImgbutton">图片</div>
+      <textarea class="text-input" name id cols="30" rows="10" placeholder="有什么新鲜事告诉大家"></textarea>
+      <div class="bottom">
+        <div class="imgButton">
+          <div class="imgButton-tiitle" @click.stop="toggleUpImgbutton">图片</div>
+          <div class="upload-img" v-show="showUpImgbutton">
+            <div class="upImg">
+              <div class="title">+</div>
+              <input type="file" multiple @change="handleImgbutton" accept=".jpg, .png" />
             </div>
-            <div class="wTouTiao-release">发布</div>
+            <!-- 上传图片的地方开始 -->
+            <div class="img-item" v-for="img in upImgs" :key="img">
+              <img :src="img" alt />
+            </div>
+            <!-- 上传图片的地方结束 -->
+          </div>
         </div>
+        <div class="wTouTiao-release">发布</div>
+      </div>
     </div>
     <div class="post-text" v-show="activeTab=='article'">写文章</div>
     <!-- 文章输入结束 -->
-</div>
+  </div>
 </template>
 
 <script>
@@ -32,105 +42,169 @@
 //例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-//import引入的组件需要注入到对象中才能使用
-components: {},
-data() {
-//这里存放数据
-return {
-    tabs:[{
-        id:"wTouTiao",
-        text:"发布微头条"
-    },{
-        id:"article",
-        text:"写文章"
-    }],
-    activeTab:"wTouTiao",
-    showUpImgbutton: false,
-    upImg:[],
-};
-},
-//监听属性 类似于data概念
-computed: {},
-//监控data中的数据变化
-watch: {},
-//方法集合
-methods: {
-    clickTab:function(tabId){
-        this.activeTab = tabId;
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+      tabs: [
+        {
+          id: "wTouTiao",
+          text: "发布微头条"
+        },
+        {
+          id: "article",
+          text: "写文章"
+        }
+      ],
+      activeTab: "wTouTiao",
+      showUpImgbutton: false,
+      upImgs:[],
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    //图片文件上传事件
+    handleImgbutton: function(e) {
+      Array.from(e.target.files).forEach(f => {
+        let params = new FormData();
+        params.append("file", f);
+
+        //发请求
+        this.$axios.post("/aliossUpload", params).then(res => {
+            console.log(res)
+          this.upImgs.push(res.url);
+        });
+      });
     },
-    toggleUpImgbutton:function(){
-        this.showUpImgbutton = !showUpImgbutton
+    //切换是否显示图片
+    toggleUpImgbutton: function() {
+      this.showUpImgbutton = !this.showUpImgbutton;
+    },
+    //切换tab栏
+    clickTab: function(tabId) {
+      this.activeTab = tabId;
     }
-},
-//生命周期 - 创建完成（可以访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（可以访问DOM元素）
-mounted() {
-
-},
-beforeCreate() {}, //生命周期 - 创建之前
-beforeMount() {}, //生命周期 - 挂载之前
-beforeUpdate() {}, //生命周期 - 更新之前
-updated() {}, //生命周期 - 更新之后
-beforeDestroy() {}, //生命周期 - 销毁之前
-destroyed() {}, //生命周期 - 销毁完成
-activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
-}
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
 </script>
 <style lang='less' scoped>
 @import "../../assets/ttIconfont.css";
-.post-box{
-    .post-tabs {
+.post-box {
+  .post-tabs {
     width: 100%;
     display: flex;
 
-  .tabs-item {
-    width: 100px;
-    font-size: 14px;
-    height: 40px;
-    margin: 0 20px;
-    text-align: center;
-    line-height: 40px;
-  }
-  .active{
+    .tabs-item {
+      width: 100px;
+      font-size: 14px;
+      height: 40px;
+      margin: 0 20px;
+      text-align: center;
+      line-height: 40px;
+    }
+    .active {
       border-bottom: 2px solid var(--themeColor);
       color: #629bfb;
+    }
   }
-}
-.post-text {
-    width:100%;
+  .post-text {
+    width: 100%;
     padding: 10px;
-  .text-input {
+    .text-input {
       width: 100%;
       border: 1px solid #ddd;
-  }
+    }
 
-  .bottom {
+    .bottom {
       height: 30px;
       display: flex;
       justify-content: space-between;
       align-items: center;
-    
-    .imgButton {
-        .imgButton-tiitle{
 
+      .imgButton {
+          position: relative;
+
+        .imgButton-tiitle {
+          font-size: 16px;
+          margin-bottom: 10px;
         }
-       .icon-picture{
-           font-size: 20px;
-       } 
-    }
+        .icon-picture {
+          font-size: 20px;
+        }
+        .upload-img {
+            width: 350px;
+            border:1px solid #ddd;
+            position: absolute;
+            padding: 15px;
+            border-radius: 5px;
+            display: flex;
+            flex-wrap: wrap;
+            
+          .upImg {
+              width: 100px;
+              height: 100px;
+              border-radius: 5px;
+              border: 1px dashed #ddd;
+              background-color: white;
+             
+            .title {
+                font-size: 40px;
+                font-weight: 600;
+                color: #ddd;
+                width: 100px;
+                height: 100px;
+                line-height: 100px;
+                text-align: center;
+            }
 
-    .wTouTiao-release {
+            input {
+                opacity: 0;
+                width: 100px;
+                height: 100px;
+                position: absolute;
+                top: 15px;
+                left: 15px;               
+            }
+          }
+
+          .img-item {
+              display: flex;
+              width: 100px;
+              height: 100px;
+              padding: 5px;
+            img {
+                width: 100%;
+                height: 100%;
+            }
+          }
+        }
+      }
+
+      .wTouTiao-release {
         width: 100px;
         height: 30px;
         line-height: 30px;
         color: white;
         background-color: var(--themeColor);
         text-align: center;
+      }
     }
   }
-}
 }
 </style>
--- toutiao/src/main.js
+++ toutiao/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//引入自定义的请求库
+import axios from "./common/axios"
+Vue.prototype.$axios = axios;
 //引入路由配置信息
 import router from "./router"
 Vue.config.productionTip = false
