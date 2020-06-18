7a4e102e67208dc3f2ec4a5c370104fed8f7ea64
zhangpingping-123 <2569228717@qq.com>
2020-06-18 20:54:10 +0800

    移动端完成

--- toutiao/src/components/news/NewsDetail.vue
+++ toutiao/src/components/news/NewsDetail.vue
@@ -1,11 +1,23 @@
 <!-- 文章内容 -->
 <template>
   <div class="tt-article-detail">
-    <div class="article-detail-left">左侧导航</div>
-    <div class="article-detail-middle">
-        <h1>{{article.title}}</h1>
+    <!-- 文章头部开始 -->
+    <div class="article-detail-header"></div>
+    <!-- 文章头部结束 -->
+    <!-- 文章內容开始 -->
+    <div class="article-detail-content">
+      <div class="article-detail-left">左侧导航</div>
+      <div class="article-detail-middle">
+        <h2>{{articles.title}}</h2>
+        <div class="article-date">
+          <div class="nickname">{{articles.nickname}}</div>
+          <div class="date">{{articles.date}}</div>
+        </div>
+        <div class="article-text" v-html="articles.content"></div>
+      </div>
+      <div class="article-detail-right">用户信息</div>
     </div>
-    <div class="article-detail-right">用户信息</div>
+    <!-- 文章内容结束 -->
   </div>
 </template>
 
@@ -18,29 +30,46 @@ export default {
   components: {},
   data() {
     //这里存放数据
-    return {};
+    return {
+      articles: {
+        nid:"",
+        title:"",
+        nickname:"",
+        date:"",
+        content:"",
+      },
+      // nid:0,
+      // nickname:"",
+    };
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {
-      //获取文章数据
-      console.log(this.$router)
-      let nid = this.$router.history.current.query.nid;
-      console.log(nid)
-      this.$axios.post("/getArticleById",{
-          nid:nid
-      }).then(res=>{
-          console.log(res)
-          this.article = res.article;
+    //获取文章数据
+    console.log(this.$router);
+    let nid = this.$router.history.current.query.nid;
+    console.log(nid);
+    this.$axios.post("/getArticleById", {
+        nid: nid
       })
-
+      .then(res => {
+        console.log(res);
+        this.articles = res.article;
+        this.articles.nid = res.article.nid;
+        this.articles.title = res.article.title   
+        this.articles.nickname = res.article.n_user.nickname
+        this.articles.date = res.article.created_at
+        this.articles.content = res.article.content
+      });
   },
   beforeCreate() {}, //生命周期 - 创建之前
   beforeMount() {}, //生命周期 - 挂载之前
@@ -48,26 +77,28 @@ export default {
   updated() {}, //生命周期 - 更新之后
   beforeDestroy() {}, //生命周期 - 销毁之前
   destroyed() {}, //生命周期 - 销毁完成
+  
   activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
 <style lang='less' scoped>
 .tt-article-detail {
+  .article-detail-content {
     display: flex;
     padding: 20px;
-  .article-detail-left {
+    .article-detail-left {
       flex: 2;
       background-color: #ddd;
-  }
+    }
 
-  .article-detail-middle {
+    .article-detail-middle {
       flex: 6;
-      
-  }
+    }
 
-  .article-detail-right {
+    .article-detail-right {
       flex: 2;
       background-color: blanchedalmond;
+    }
   }
 }
 </style>
